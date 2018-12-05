fa1e08662cf74e3b3c85ef61de52aec16c67de71
Ragnar-H <ragnarhardarson90@gmail.com>
2018-12-05 21:32:23 +0100

    Try loading the model when we start the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { loadModel, isModelLoaded } from "./Model";
 
 class App extends Component {
+  state = {
+    isModelLoaded: false
+  };
+
+  async componentDidMount() {
+    await loadModel();
+    this.setState({
+      isModelLoaded: isModelLoaded()
+    });
+  }
+
   render() {
+    const { isModelLoaded } = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <p>Model loaded : {isModelLoaded ? "it is loaded" : "not yet"}</p>
       </div>
     );
   }
